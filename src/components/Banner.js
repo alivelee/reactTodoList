489549084f10c71d91c8484e5223d50eabd68193
Xu Lei <a871460434@gmail.com>
2016-09-25 11:48:35 +0800

    add banner

---
+++ src/components/Banner.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class Banner extends Component {
+  render() {
+    return (
+      <p>ToDO App Powered By React</p>
+    );
+  }
+}
+export default Banner;
